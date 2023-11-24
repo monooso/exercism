762d02dbf61ce6ce56ed6d2dd9d6ef6fc0df4ece
Stephen Lewis <gpg@stephenlewis.me>
2023-11-24 21:39:54 +0000

    Implement solution to factory-sensors exercise

---
+++ javascript/factory-sensors/factory-sensors.js
@@ -0,0 +1,62 @@
+// @ts-check
+
+export class ArgumentError extends Error { }
+
+export class OverheatingError extends Error {
+  /**
+   * @param {number} temperature
+   */
+  constructor(temperature) {
+    super(`The temperature is ${temperature} ! Overheating !`);
+    this.temperature = temperature;
+  }
+}
+
+/**
+ * Check if the humidity level is not too high.
+ *
+ * @param {number} humidityPercentage
+ * @throws {Error}
+ */
+export function checkHumidityLevel(humidityPercentage) {
+  if (humidityPercentage > 70) { throw new Error('Humidity level too high !'); }
+}
+
+/**
+ * Check if the temperature is not too high.
+ *
+ * @param {number|null} temperature
+ * @throws {ArgumentError|OverheatingError}
+ */
+export function reportOverheating(temperature) {
+  if (temperature === null) throw new ArgumentError('No temperature provided !');
+  if (temperature > 500) throw new OverheatingError(temperature);
+}
+
+/**
+ *  Triggers the needed action depending on the result of the machine check.
+ *
+ * @param {{
+ * check: function,
+ * alertDeadSensor: function,
+ * alertOverheating: function,
+ * shutdown: function
+ * }} actions
+ * @throws {ArgumentError|OverheatingError|Error}
+ */
+export function monitorTheMachine(actions) {
+  try {
+    actions.check();
+  } catch (error) {
+    switch (error.constructor) {
+      case ArgumentError:
+        actions.alertDeadSensor();
+        break;
+      case OverheatingError:
+        error.temperature > 600 ? actions.shutdown() : actions.alertOverheating();
+        break;
+      default:
+        throw error;
+    }
+  }
+}
